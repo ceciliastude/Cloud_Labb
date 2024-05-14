ca9c8780a30fdc3fc63b48339b081862f7a3c33f
MajaJoh <maja.johansson@ha.ax>
2024-05-14 04:21:32 +0300

    bucket policy wip

--- src/main/java/ax/ha/clouddevelopment/WebsiteBucketStack.java
+++ src/main/java/ax/ha/clouddevelopment/WebsiteBucketStack.java
@@ -1,9 +1,15 @@
 package ax.ha.clouddevelopment;
 
 import software.amazon.awscdk.*;
+import software.amazon.awscdk.services.iam.AnyPrincipal;
+import software.amazon.awscdk.services.iam.Effect;
 import software.amazon.awscdk.services.s3.BlockPublicAccess;
 import software.amazon.awscdk.services.s3.Bucket;
 import software.constructs.Construct;
+import software.amazon.awscdk.services.iam.PolicyStatement;
+
+import java.util.List;
+import java.util.Map;
 
 
 public class WebsiteBucketStack extends Stack {
@@ -17,6 +23,7 @@ public class WebsiteBucketStack extends Stack {
                               final String groupName) {
         super(scope, id, props);
         // S3 Bucket resource
+
         final Bucket siteBucket =
                 Bucket.Builder.create(this, "websiteBucket")
                         .bucketName(groupName + "-website")
@@ -26,12 +33,28 @@ public class WebsiteBucketStack extends Stack {
                         .autoDeleteObjects(true)
                         .websiteIndexDocument("index.html")
                         .build();
+
+        PolicyStatement sitePolicyStatement = PolicyStatement.Builder.create()
+                .actions(List.of("s3:GetObject"))
+                .resources(List.of(siteBucket.getBucketArn() + "/*"))
+                .effect(Effect.ALLOW)
+                .principals(List.of(new AnyPrincipal()))
+                .conditions(Map.of(
+                        "IpAddress", Map.of("aws:SourceIp", List.of("194.132.145.78/32"))
+                ))
+                .build();
+
+        siteBucket.addToResourcePolicy(PolicyStatement.Builder.create().build());
+
+
         // Bucket ARN CfnOutput variable. This is helpful for a later stage when simply wanting
         // to refer to your storage bucket using only a simple variable
         CfnOutput.Builder.create(this, "websiteBucketOutput")
                 .description(String.format("URL of your bucket.", groupName))
                 .value(siteBucket.getBucketWebsiteUrl())
                 .exportName(groupName + "-s3-demo-url")
+
                 .build();
+
     }
 }
